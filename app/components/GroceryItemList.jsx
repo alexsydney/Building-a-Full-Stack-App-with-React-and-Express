8b26e74edec4a91b41dc75ec85174b25c91ab156
Jorge Núñez Siri <jnunez@intellisys.com.do>
2016-06-06 11:44:55 -0400

    Scaffolding the App: gulpfile, browserify and first react component.

---
+++ app/components/GroceryItemList.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+var GroceryItemList = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Grocery Listify</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = GroceryItemList;
