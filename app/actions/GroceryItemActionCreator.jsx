03e7c331afca07233ab9c517e663a935ac375474
Jorge Núñez Siri <jnunez@intellisys.com.do>
2016-06-20 11:53:37 -0400

    Completing the Front End: Adding buy/unbuy and delete actions and stores. Adding some css.

--- app/actions/GroceryItemActionCreator.jsx
+++ app/actions/GroceryItemActionCreator.jsx
@@ -8,5 +8,23 @@ module.exports = {
       payload:item,
       type:"grocery-item:add"
     })
+  },
+  buy: function(item){
+    dispatcher.dispatch({
+      payload:item,
+      type:"grocery-item:buy"
+    })
+  },
+  delete: function(item){
+    dispatcher.dispatch({
+      payload:item,
+      type:"grocery-item:delete"
+    })
+  },
+  unbuy: function(item){
+    dispatcher.dispatch({
+      payload:item,
+      type:"grocery-item:unbuy"
+    })
   }
 }
